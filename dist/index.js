55a4c6a904a2049b8c7f5da3ca79cbe9ec85d872
ryugo-abe <abe.ryugo@gmail.com>
2024-04-19 12:30:22 +0900

    do not install

--- dist/index.js
+++ dist/index.js
@@ -130,9 +130,9 @@ function run() {
     var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k;
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const installer = new installer_1.Installer();
-            const version = (_a = core.getInput('version')) !== null && _a !== void 0 ? _a : '';
-            yield installer.install(version);
+            // const installer = new installer_1.Installer();
+            // const version = (_a = core.getInput('version')) !== null && _a !== void 0 ? _a : '';
+            // yield installer.install(version);
             const solutionPath = core.getInput('solutionPath');
             const outputPath = 'result.xml';
             let command = `jb inspectcode --output=${outputPath} --absolute-paths ${solutionPath}`;
